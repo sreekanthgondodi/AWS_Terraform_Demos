4a1545255dd0a434290aa411e12dfffc1a8a4024
EC2 Default User <ec2-user@ip-172-31-25-241.ec2.internal>
2022-01-31 08:23:27 +0000

    Initial Commit

---
+++ Variables_Demo/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "variable_demo" {
+  
+ count = 1 
+ ami = "${lookup(var.ami_type, var.region)}"
+ 
+ instance_type = "${lookup(var.instance_type, var.env)}" 
+  tags = {
+     Name = "Terraform_Demo_Instance - ${count.index}"
+  }
+
+
+}
